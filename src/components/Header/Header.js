5acfd0e9f4933ddfde6e96b0e0c248add629726e
JOsktgui <jose.osk01@gmail.com>
2018-01-23 11:15:30 -0600

    structure

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+// dependencies
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render () {
+    return (
+      <div>
+        HR
+      </div>
+    )
+  }
+}
+
+export default Header;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+// dependencies
+import React, { Component } from 'react';
+
+// components
+
+class Home extends Component {
+  render () {
+    return (
+      <div>
+        Home
+      </div>
+    )
+  }
+}
+
+export default Home;
